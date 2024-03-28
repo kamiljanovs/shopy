7ded7df8fca99169721ed72a7c2040458074c953
Adilet <adiletkamiljanov@gmail.com>
2024-03-28 19:06:07 +0600

    fourth lesson

--- product/admin.py
+++ product/admin.py
@@ -2,7 +2,42 @@ from django.contrib import admin
 
 from product.models import Product, Category, Review, Tag
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Review)
-admin.site.register(Tag)
+# admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'size')
+    search_fields = ('title', 'price')
+
+    def save_model(self, request, obj, form, change):
+        obj.title = obj.title.capitalize()
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'product_id')
+    search_fields = ('name', 'product_id')
+
+    def save_model(self, request, obj, form, change):
+        obj.title = obj.title.capitalize()
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('product_id', 'text', 'id')
+    search_fields = ('text', 'product_id')
+
+    def save_model(self, request, obj, form, change):
+        obj.title = obj.title.capitalize()
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id','text')
+    search_fields = ('text', 'id')
+
+    def save_model(self, request, obj, form, change):
+        obj.title = obj.title.capitalize()
+        super().save_model(request, obj, form, change)
--- shop/urls.py
+++ shop/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from product.views import hello_view, current_date_view, goodby_view, product_list_view, main_page_view,\
     product_detail_view, category_list_view
 
@@ -27,4 +30,4 @@ urlpatterns = [
     path('products/', product_list_view, name='product_list'),
     path('products/<int:product_id>/', product_detail_view, name='product_detail'),
     path('categories/', category_list_view, name='category_list')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
